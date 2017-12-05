4d8d97100e77a7907a56d6f392064eeb1ed951e9
piotrheliosz <piotr.heliosz@gmail.com>
2017-12-05 15:18:29 +0100

    firefox driver

--- src/test/java/Scenario.java
+++ src/test/java/Scenario.java
@@ -2,6 +2,7 @@ import org.junit.After;
 import org.junit.Before;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 
 public class Scenario {
 
@@ -11,8 +12,11 @@ public class Scenario {
     public void setUp() {
 
         System.setProperty("webdriver.chrome.driver", "C:\\workspace\\automationpractice\\chromedriver.exe");
+        System.setProperty( "webdriver.firefox.marionette", "C:\\workspace\\automationpractice\\geckodriver.exe" );
 
-        driver = new ChromeDriver();
+        //driver = new ChromeDriver();
+        driver = new FirefoxDriver(); //ver 48.0b2
+
         driver.manage().window().maximize();
         driver.get("http://automationpractice.com");
 
